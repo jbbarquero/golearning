0e0cb9447910659b89d58990aaaf22737d86bfb3
Javier Beneito Barquero <jbbarquero@gmail.com>
2018-03-23 23:17:01 +0100

    golearning. How to write Go code, first program. See https://golang.org/doc/code.html

---
+++ hallo/hallo.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jbbarquero/golearning/stringutil"
+)
+
+func main() {
+	fmt.Printf("Hallo Welt\n")
+	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+	fmt.Printf("\n")
+}
